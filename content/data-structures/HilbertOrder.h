c4ed4dc673c940b70a551eb5232427789d4ad333
Kushagra <1kushagra1@gmail.com>
2024-04-08 20:05:14 +0530

    update

--- content/data-structures/HilbertOrder.h
+++ content/data-structures/HilbertOrder.h
@@ -9,8 +9,8 @@
 
 #pragma once
 
-lol hilbertorder (int x, int y) {
-    lol o = 0; const int mx = 1 << 20;
+ll hilbertorder (int x, int y) {
+    ll o = 0; const int mx = 1 << 20;
     for (int p = mx; p; p >>= 1){
         bool a = x&p, b = y&p;
         o = (o << 2) | (a * 3) ^ static_cast<int>(b);
