a538e10e5d7dd965f924cf386199d68ab921e8fe
Kushagra <1kushagra1@gmail.com>
2024-04-10 15:53:35 +0530

    update

--- content/math/xor_basis.h
+++ content/math/xor_basis.h
@@ -9,4 +9,37 @@
 
 #pragma once
 
-ggggg
+struct Basis {
+    int bits = 30;
+    array<int, 30> basis;
+    Basis(){
+      for(int i=0;i<bits;i++){
+          basis[i]=0;
+      }
+    }
+    void add(int x) {
+        for (int i = bits-1; i >= 0 && x > 0; --i) {
+            if (basis[i]) x = min(x, x ^ basis[i]);
+            else {basis[i] = x; x = 0;}
+        }
+    }
+    void merge(const Basis &other) {
+        for (int i = bits-1; i >= 0; --i) {
+            if (!other.basis[i]) break;
+            add(other.basis[i]);
+        }
+    }
+    int getmax() {
+        int ret = 0;
+        for (int i = bits-1; i >= 0; --i) {
+            ret = max(ret, ret ^ basis[i]);
+        }
+        return ret;
+    }
+    bool isPossible(int k) const {
+        for (int i = bits-1; i >= 0; --i) {
+            k = min(k, k ^ basis[i]);
+        }
+        return k == 0;
+    }
+};
